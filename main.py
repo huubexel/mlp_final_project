20488a42624ed042b475210fb8e415f74c34fa0b
huubexel <huubexel@hotmail.com>
2024-03-19 15:06:28 +0100

    Added reading in the file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# This is the main file
+from read_data import read_data_from_csv_file
 
 
 def main():
-    print("hoi")
-    print("doei")
-    print("moi")
+    training_data = read_data_from_csv_file("data/train_data_offensive_taskA.csv")
 
 
 if __name__ == "__main__":
