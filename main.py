7c68bd60911b739a6b3bc423c1f8075a6030edf6
huubexel <huubexel@hotmail.com>
2024-03-19 16:41:40 +0100

    Added preprocessing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from read_data import read_data_from_csv_file
+from preprocessing import preprocesses_data
 
 
 def main():
     training_data = read_data_from_csv_file("data/train_data_offensive_taskA.csv")
+    preprocessed_data = preprocesses_data(training_data)
 
 
 if __name__ == "__main__":
