4ab2284401700c6cef6d2e7c64a1a94c4b2bb43d
huubexel <huubexel@hotmail.com>
2024-03-24 18:04:11 +0100

    Moi

--- bert.py
+++ bert.py
@@ -1,27 +1,21 @@
-from torch import no_grad, Tensor, device
-from torch.cuda import is_available, empty_cache
+from torch import no_grad
 from transformers import BertModel
 
 
-def get_bert_model(bert_model_to_use: str):
-    # If you have a GPU that has cuda and has that enabled run it on there, otherwise use the CPU
-    device_to_train_on = device('cuda' if is_available() else 'cpu')
-
-    empty_cache()
-
+def get_bert_model(bert_model_to_use: str, device_to_run_on):
     # Get the pretrained model
-    model = BertModel.from_pretrained(bert_model_to_use, output_hidden_states=True).to(device_to_train_on)
+    model = BertModel.from_pretrained(bert_model_to_use).to(device_to_run_on)
 
     # This sets the training mode on false, so BERT won't retrain itself, which is what we want
     model.eval()
 
-    return model, device_to_train_on
+    return model
 
 
-def get_bert_embeddings(model, device_to_train_on: str, tokens_tensor: Tensor, segments_tensor: Tensor):
+def get_bert_embeddings(model, tokenized_tweets, device_to_run_on):
     with no_grad():
 
         # Run the data through BERT
-        outputs = model(tokens_tensor.to(device_to_train_on), segments_tensor.to(device_to_train_on))
+        outputs = model(**tokenized_tweets).to(device_to_run_on)
 
-        return outputs[2][11]
+        return outputs.last_hidden_state
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from read_data import read_data_from_csv_file
-from preprocessing import preprocesses_data, preprocessed_data_for_bert, get_labels
+from preprocessing import preprocesses_data, preprocessed_data_for_bert
 from bert import get_bert_model, get_bert_embeddings
 from embeddings import write_to_file
+from torch import device
+from torch.cuda import is_available
 from bagging import bagging
 
 
 def main():
-    # Preprocessing and setting up data for later usage
-    bert_model_to_use = "bert-base-uncased"
-    training_data = read_data_from_csv_file("data/train_data_offensive_taskA.csv")
-    preprocessed_data = preprocesses_data(training_data)
-    labels = get_labels(training_data)
+    # Get the device to run on which is either the cpu or the cuda cores from the gpu
+    # device_to_run_on = device('cuda' if is_available() else 'cpu')
+    device_to_run_on = 'cpu'
 
-    # BERT
-    tokens_tensor, segments_tensor = preprocessed_data_for_bert(preprocessed_data, bert_model_to_use)
-    bert_model, device_to_train_on = get_bert_model(bert_model_to_use)
-    embeddings = get_bert_embeddings(bert_model, device_to_train_on, tokens_tensor, segments_tensor)
+    # Preprocessing and setting up data for later usage
+    bert_model_to_use = "google-bert/bert-base-uncased"
+    training_data = read_data_from_csv_file("data/train_data_offensive_taskA.csv")
+    preprocessed_data, labels = preprocesses_data(training_data)
+
+    tokenized_tweets = preprocessed_data_for_bert(preprocessed_data, bert_model_to_use, device_to_run_on)
+    bert_model = get_bert_model(bert_model_to_use, device_to_run_on)
+
+    embeddings = get_bert_embeddings(bert_model, tokenized_tweets, device_to_run_on)
     write_to_file(embeddings)
 
     # Ensemble model
-    bagging(embeddings, labels)
+    # bagging(embeddings, labels)
 
 
 if __name__ == "__main__":
